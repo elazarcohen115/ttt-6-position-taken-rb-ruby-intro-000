f670e44effbcf9b5c0ae18d6f879b354dfcc7f27
elazar cohen <escohen115@gmail.com>
2020-04-07 14:23:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 # code your #position_taken? method here!
 def position_taken? (board, position)
-if board[position] == " " || board[position] == "" || board[position] == nil
+if board[position] == " " || board[position] == || board[position] == nil
   return false
 else
   return true
-end
